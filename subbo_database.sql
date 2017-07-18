011014708aa70c84afefce185eb5e23150d395e8
arz1893 <arz1893@gmail.com>
2017-07-18 16:30:18 +0700

    first update

--- subbo_database.sql
+++ subbo_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 05, 2017 at 06:04 AM
+-- Generation Time: Jul 18, 2017 at 11:28 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -87,6 +87,157 @@ INSERT INTO `categories` (`id`, `category_name`, `description`, `image`, `create
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `currency`
+--
+
+CREATE TABLE `currency` (
+  `id` int(10) NOT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `currency` varchar(100) DEFAULT NULL,
+  `code` varchar(100) DEFAULT NULL,
+  `symbol` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `currency`
+--
+
+INSERT INTO `currency` (`id`, `country`, `currency`, `code`, `symbol`) VALUES
+(1, 'Albania', 'Leke', 'ALL', 'Lek'),
+(2, 'America', 'Dollars', 'USD', '$'),
+(3, 'Afghanistan', 'Afghanis', 'AFN', '?'),
+(4, 'Argentina', 'Pesos', 'ARS', '$'),
+(5, 'Aruba', 'Guilders', 'AWG', 'ƒ'),
+(6, 'Australia', 'Dollars', 'AUD', '$'),
+(7, 'Azerbaijan', 'New Manats', 'AZN', '???'),
+(8, 'Bahamas', 'Dollars', 'BSD', '$'),
+(9, 'Barbados', 'Dollars', 'BBD', '$'),
+(10, 'Belarus', 'Rubles', 'BYR', 'p.'),
+(11, 'Belgium', 'Euro', 'EUR', '€'),
+(12, 'Beliz', 'Dollars', 'BZD', 'BZ$'),
+(13, 'Bermuda', 'Dollars', 'BMD', '$'),
+(14, 'Bolivia', 'Bolivianos', 'BOB', '$b'),
+(15, 'Bosnia and Herzegovina', 'Convertible Marka', 'BAM', 'KM'),
+(16, 'Botswana', 'Pula', 'BWP', 'P'),
+(17, 'Bulgaria', 'Leva', 'BGN', '??'),
+(18, 'Brazil', 'Reais', 'BRL', 'R$'),
+(19, 'Britain (United Kingdom)', 'Pounds', 'GBP', '£'),
+(20, 'Brunei Darussalam', 'Dollars', 'BND', '$'),
+(21, 'Cambodia', 'Riels', 'KHR', '?'),
+(22, 'Canada', 'Dollars', 'CAD', '$'),
+(23, 'Cayman Islands', 'Dollars', 'KYD', '$'),
+(24, 'Chile', 'Pesos', 'CLP', '$'),
+(25, 'China', 'Yuan Renminbi', 'CNY', '¥'),
+(26, 'Colombia', 'Pesos', 'COP', '$'),
+(27, 'Costa Rica', 'Colón', 'CRC', '?'),
+(28, 'Croatia', 'Kuna', 'HRK', 'kn'),
+(29, 'Cuba', 'Pesos', 'CUP', '?'),
+(30, 'Cyprus', 'Euro', 'EUR', '€'),
+(31, 'Czech Republic', 'Koruny', 'CZK', 'K?'),
+(32, 'Denmark', 'Kroner', 'DKK', 'kr'),
+(33, 'Dominican Republic', 'Pesos', 'DOP ', 'RD$'),
+(34, 'East Caribbean', 'Dollars', 'XCD', '$'),
+(35, 'Egypt', 'Pounds', 'EGP', '£'),
+(36, 'El Salvador', 'Colones', 'SVC', '$'),
+(37, 'England (United Kingdom)', 'Pounds', 'GBP', '£'),
+(38, 'Euro', 'Euro', 'EUR', '€'),
+(39, 'Falkland Islands', 'Pounds', 'FKP', '£'),
+(40, 'Fiji', 'Dollars', 'FJD', '$'),
+(41, 'France', 'Euro', 'EUR', '€'),
+(42, 'Ghana', 'Cedis', 'GHC', '¢'),
+(43, 'Gibraltar', 'Pounds', 'GIP', '£'),
+(44, 'Greece', 'Euro', 'EUR', '€'),
+(45, 'Guatemala', 'Quetzales', 'GTQ', 'Q'),
+(46, 'Guernsey', 'Pounds', 'GGP', '£'),
+(47, 'Guyana', 'Dollars', 'GYD', '$'),
+(48, 'Holland (Netherlands)', 'Euro', 'EUR', '€'),
+(49, 'Honduras', 'Lempiras', 'HNL', 'L'),
+(50, 'Hong Kong', 'Dollars', 'HKD', '$'),
+(51, 'Hungary', 'Forint', 'HUF', 'Ft'),
+(52, 'Iceland', 'Kronur', 'ISK', 'kr'),
+(53, 'India', 'Rupees', 'INR', 'Rp'),
+(54, 'Indonesia', 'Rupiahs', 'IDR', 'Rp'),
+(55, 'Iran', 'Rials', 'IRR', '?'),
+(56, 'Ireland', 'Euro', 'EUR', '€'),
+(57, 'Isle of Man', 'Pounds', 'IMP', '£'),
+(58, 'Israel', 'New Shekels', 'ILS', '?'),
+(59, 'Italy', 'Euro', 'EUR', '€'),
+(60, 'Jamaica', 'Dollars', 'JMD', 'J$'),
+(61, 'Japan', 'Yen', 'JPY', '¥'),
+(62, 'Jersey', 'Pounds', 'JEP', '£'),
+(63, 'Kazakhstan', 'Tenge', 'KZT', '??'),
+(64, 'Korea (North)', 'Won', 'KPW', '?'),
+(65, 'Korea (South)', 'Won', 'KRW', '?'),
+(66, 'Kyrgyzstan', 'Soms', 'KGS', '??'),
+(67, 'Laos', 'Kips', 'LAK', '?'),
+(68, 'Latvia', 'Lati', 'LVL', 'Ls'),
+(69, 'Lebanon', 'Pounds', 'LBP', '£'),
+(70, 'Liberia', 'Dollars', 'LRD', '$'),
+(71, 'Liechtenstein', 'Switzerland Francs', 'CHF', 'CHF'),
+(72, 'Lithuania', 'Litai', 'LTL', 'Lt'),
+(73, 'Luxembourg', 'Euro', 'EUR', '€'),
+(74, 'Macedonia', 'Denars', 'MKD', '???'),
+(75, 'Malaysia', 'Ringgits', 'MYR', 'RM'),
+(76, 'Malta', 'Euro', 'EUR', '€'),
+(77, 'Mauritius', 'Rupees', 'MUR', '?'),
+(78, 'Mexico', 'Pesos', 'MXN', '$'),
+(79, 'Mongolia', 'Tugriks', 'MNT', '?'),
+(80, 'Mozambique', 'Meticais', 'MZN', 'MT'),
+(81, 'Namibia', 'Dollars', 'NAD', '$'),
+(82, 'Nepal', 'Rupees', 'NPR', '?'),
+(83, 'Netherlands Antilles', 'Guilders', 'ANG', 'ƒ'),
+(84, 'Netherlands', 'Euro', 'EUR', '€'),
+(85, 'New Zealand', 'Dollars', 'NZD', '$'),
+(86, 'Nicaragua', 'Cordobas', 'NIO', 'C$'),
+(87, 'Nigeria', 'Nairas', 'NGN', '?'),
+(88, 'North Korea', 'Won', 'KPW', '?'),
+(89, 'Norway', 'Krone', 'NOK', 'kr'),
+(90, 'Oman', 'Rials', 'OMR', '?'),
+(91, 'Pakistan', 'Rupees', 'PKR', '?'),
+(92, 'Panama', 'Balboa', 'PAB', 'B/.'),
+(93, 'Paraguay', 'Guarani', 'PYG', 'Gs'),
+(94, 'Peru', 'Nuevos Soles', 'PEN', 'S/.'),
+(95, 'Philippines', 'Pesos', 'PHP', 'Php'),
+(96, 'Poland', 'Zlotych', 'PLN', 'z?'),
+(97, 'Qatar', 'Rials', 'QAR', '?'),
+(98, 'Romania', 'New Lei', 'RON', 'lei'),
+(99, 'Russia', 'Rubles', 'RUB', '???'),
+(100, 'Saint Helena', 'Pounds', 'SHP', '£'),
+(101, 'Saudi Arabia', 'Riyals', 'SAR', '?'),
+(102, 'Serbia', 'Dinars', 'RSD', '???.'),
+(103, 'Seychelles', 'Rupees', 'SCR', '?'),
+(104, 'Singapore', 'Dollars', 'SGD', '$'),
+(105, 'Slovenia', 'Euro', 'EUR', '€'),
+(106, 'Solomon Islands', 'Dollars', 'SBD', '$'),
+(107, 'Somalia', 'Shillings', 'SOS', 'S'),
+(108, 'South Africa', 'Rand', 'ZAR', 'R'),
+(109, 'South Korea', 'Won', 'KRW', '?'),
+(110, 'Spain', 'Euro', 'EUR', '€'),
+(111, 'Sri Lanka', 'Rupees', 'LKR', '?'),
+(112, 'Sweden', 'Kronor', 'SEK', 'kr'),
+(113, 'Switzerland', 'Francs', 'CHF', 'CHF'),
+(114, 'Suriname', 'Dollars', 'SRD', '$'),
+(115, 'Syria', 'Pounds', 'SYP', '£'),
+(116, 'Taiwan', 'New Dollars', 'TWD', 'NT$'),
+(117, 'Thailand', 'Baht', 'THB', '?'),
+(118, 'Trinidad and Tobago', 'Dollars', 'TTD', 'TT$'),
+(119, 'Turkey', 'Lira', 'TRY', 'TL'),
+(120, 'Turkey', 'Liras', 'TRL', '£'),
+(121, 'Tuvalu', 'Dollars', 'TVD', '$'),
+(122, 'Ukraine', 'Hryvnia', 'UAH', '?'),
+(123, 'United Kingdom', 'Pounds', 'GBP', '£'),
+(124, 'United States of America', 'Dollars', 'USD', '$'),
+(125, 'Uruguay', 'Pesos', 'UYU', '$U'),
+(126, 'Uzbekistan', 'Sums', 'UZS', '??'),
+(127, 'Vatican City', 'Euro', 'EUR', '€'),
+(128, 'Venezuela', 'Bolivares Fuertes', 'VEF', 'Bs'),
+(129, 'Vietnam', 'Dong', 'VND', '?'),
+(130, 'Yemen', 'Rials', 'YER', '?'),
+(131, 'Zimbabwe', 'Zimbabwe Dollars', 'ZWD', 'Z$');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `image_thumbnails`
 --
 
@@ -99,7 +250,8 @@ CREATE TABLE `image_thumbnails` (
   `image_id` int(10) UNSIGNED NOT NULL,
   `album_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `user_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -142,7 +294,27 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (5, '2017_06_28_095004_create_categories_table', 3),
 (9, '2017_06_28_100335_create_album_category_table', 4),
 (10, '2017_06_28_100626_create_images_table', 4),
-(11, '2017_06_28_101903_create_image_thumbnails_table', 4);
+(11, '2017_06_28_101903_create_image_thumbnails_table', 4),
+(13, '2017_07_06_072526_add_columns_to_users', 5),
+(14, '2017_07_13_084332_add_instagram_id_to_users', 6),
+(16, '2017_07_14_085801_create_wallets_table', 7),
+(17, '2017_07_14_090112_add_wallet_id_to_users', 8),
+(18, '2017_07_14_092411_create_order_history_table', 9),
+(19, '2017_07_17_043858_add_user_id_to_image_thumbnails', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_history`
+--
+
+CREATE TABLE `order_history` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `album_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -173,16 +345,44 @@ CREATE TABLE `users` (
   `about` text COLLATE utf8mb4_unicode_ci,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `bank_name` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `account_number` char(19) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `currency_id` int(11) DEFAULT NULL,
+  `instagram_id` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `wallet_id` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `provider_name`, `provider_id`, `avatar`, `about`, `remember_token`, `created_at`, `updated_at`) VALUES
-('45a113ac-c7f2-30b0-90a5-a399ab912716', 'John Doe', 'johndoe@example.com', '$2y$10$ob59BvDc49/oIJ6ZMVggBOblVooAm.ijJ01c/sOjUfJhLUTVNgyzi', NULL, NULL, 'johndoe@example.com_Audi R8 6.jpg', 'My name is John Doe', 'etEK7aUbN7kIkucGjfEMJDzrFze0OyG3AgyYD5sy8YS8jqsU6NmfT3RhZ73N', '2017-06-29 02:11:45', '2017-06-29 02:12:50'),
-('f3c16e77-3a72-37fb-9a22-c6e6d3fb7114', 'Arnadi Denanda Surya', 'arz1893@gmail.com', NULL, 'facebook', '10210502862241008', NULL, 'Hello World!', 'NWlegAInHrqT2sWjgjlK6HqebqYdolQdkHyENZfM6EP2swNGIGFcEQotbzYe', '2017-06-28 20:37:12', '2017-06-29 01:58:25');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `provider_name`, `provider_id`, `avatar`, `about`, `remember_token`, `created_at`, `updated_at`, `bank_name`, `account_number`, `currency_id`, `instagram_id`, `wallet_id`) VALUES
+('45a113ac-c7f2-30b0-90a5-a399ab912716', 'John Doe', 'johndoe@example.com', '$2y$10$6XvuN.Sl50FLEiPsW6Vd9eIcLtVvLlS2XsxGIHD6hUTwOFjZV1BvO', NULL, NULL, 'johndoe@example.com_chef-icon.png', 'My name is John Doe, updated from firefox', 'xRn124Of3BQyX8N3UOPRll1046lp9C71heZmHvHvATazMMlFXud8kTaxylib', '2017-06-29 02:11:45', '2017-07-12 02:38:26', 'BCA', '13245498724', 54, NULL, 1),
+('f3c16e77-3a72-37fb-9a22-c6e6d3fb7114', 'Arnadi Denanda Surya', 'arz1893@gmail.com', '$2y$10$FEmbuOfBDgYLGpm0BaH1E.tBP0DxTRHZ8xgQ45vkFBML6cIQrbzbO', 'facebook', '10210502862241008', NULL, 'Hello World!, updated on 11 July 2017', 'HAq7sPcPgWhcdupLuoIx8xTFqFKvgyAvQznDNn0LWVMnEo1tXen2nVqZuhiz', '2017-06-28 20:37:12', '2017-07-11 03:26:40', 'BCA', '158792316498723', 54, NULL, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wallets`
+--
+
+CREATE TABLE `wallets` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `deposit` double NOT NULL DEFAULT '0',
+  `withdraw` double NOT NULL DEFAULT '0',
+  `user_id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `wallets`
+--
+
+INSERT INTO `wallets` (`id`, `deposit`, `withdraw`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 0, 0, '45a113ac-c7f2-30b0-90a5-a399ab912716', NULL, '2017-07-17 21:11:38'),
+(2, 0, 0, 'f3c16e77-3a72-37fb-9a22-c6e6d3fb7114', NULL, '2017-07-18 01:39:27');
 
 --
 -- Indexes for dumped tables
@@ -209,12 +409,20 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `currency`
+--
+ALTER TABLE `currency`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `currency` (`currency`);
+
+--
 -- Indexes for table `image_thumbnails`
 --
 ALTER TABLE `image_thumbnails`
   ADD PRIMARY KEY (`id`),
   ADD KEY `image_thumbnails_image_id_foreign` (`image_id`),
-  ADD KEY `image_thumbnails_album_id_foreign` (`album_id`);
+  ADD KEY `image_thumbnails_album_id_foreign` (`album_id`),
+  ADD KEY `image_thumbnails_user_id_foreign` (`user_id`);
 
 --
 -- Indexes for table `images`
@@ -230,6 +438,14 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `order_history`
+--
+ALTER TABLE `order_history`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_history_user_id_foreign` (`user_id`),
+  ADD KEY `order_history_album_id_foreign` (`album_id`);
+
+--
 -- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
@@ -240,7 +456,16 @@ ALTER TABLE `password_resets`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD KEY `users_currency_id_foreign` (`currency_id`),
+  ADD KEY `users_wallet_id_foreign` (`wallet_id`);
+
+--
+-- Indexes for table `wallets`
+--
+ALTER TABLE `wallets`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `wallets_user_id_foreign` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -252,20 +477,35 @@ ALTER TABLE `users`
 ALTER TABLE `categories`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
+-- AUTO_INCREMENT for table `currency`
+--
+ALTER TABLE `currency`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
+--
 -- AUTO_INCREMENT for table `image_thumbnails`
 --
 ALTER TABLE `image_thumbnails`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=245;
 --
 -- AUTO_INCREMENT for table `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=251;
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+--
+-- AUTO_INCREMENT for table `order_history`
+--
+ALTER TABLE `order_history`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `wallets`
+--
+ALTER TABLE `wallets`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
@@ -288,13 +528,34 @@ ALTER TABLE `albums`
 --
 ALTER TABLE `image_thumbnails`
   ADD CONSTRAINT `image_thumbnails_album_id_foreign` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `image_thumbnails_image_id_foreign` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `image_thumbnails_image_id_foreign` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `image_thumbnails_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `images`
 --
 ALTER TABLE `images`
   ADD CONSTRAINT `images_album_id_foreign` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `order_history`
+--
+ALTER TABLE `order_history`
+  ADD CONSTRAINT `order_history_album_id_foreign` FOREIGN KEY (`album_id`) REFERENCES `albums` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_history_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_currency_id_foreign` FOREIGN KEY (`currency_id`) REFERENCES `currency` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `users_wallet_id_foreign` FOREIGN KEY (`wallet_id`) REFERENCES `wallets` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `wallets`
+--
+ALTER TABLE `wallets`
+  ADD CONSTRAINT `wallets_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
